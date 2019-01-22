67dba9bab8e1e5e731e2df6f6456e1a3603af915
okpakomarvis <40584739+okpakomarvis@users.noreply.github.com>
2019-01-22 12:46:57 +0100

    Update connect.js
    add heroku db url

--- server/db/connect.js
+++ server/db/connect.js
@@ -5,14 +5,7 @@ dotenv.config();
 
 const { User, Database, Password, DBPort } = process.env;
 const connectionString =
- " postgres://eyyjuvsm:xaldVsNFMdVZAo4yGXY62IC6flByTs65@stampy.db.elephantsql.com:5432/rugby";
-
- 
-
-  
-  
-  
- 
+ "postgres://tfwgnrzxquzceg:905cc02e26d3cba3ec2cdf2e559b79643dbc28fd591555b9242fb482449ca055@ec2-54-235-68-3.compute-1.amazonaws.com:5432/d7ot1a16k184d3";
 
 const pool = new Pool({
   connectionString: connectionString
